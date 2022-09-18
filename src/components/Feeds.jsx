8d7b092c531fa1ad11cfa9b57c2b9cd655f5958a
ahmedhussin1 <ahmed208087@gmail.com>
2022-09-18 23:37:15 +0200

    sidebar added and styled

--- src/components/Feeds.jsx
+++ src/components/Feeds.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { borderRight } from "@mui/system";
+import Sidebar from "./Sidebar";
 
-const Feeds = () => {
-  return (
-    <div>Feeds</div>
-  )
-}
+const Feeds = () => (
+  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+    <Box
+      sx={{
+        height: { sx: "auto", md: "92vh" },
+        borderRight: "1px solid #3d3d3d",
+        px: { sx: 0, md: 2 },
+      }}
+    >
 
-export default Feeds
+    <Sidebar/>
+
+      <Typography
+        className="copy-right"
+        variant="body2"
+        sx={{ mt: 1.5, color: "#fff" }}
+      >
+        Copyright 2022 FakeTube.com
+      </Typography>
+      
+    </Box>
+  </Stack>
+);
+
+export default Feeds;
